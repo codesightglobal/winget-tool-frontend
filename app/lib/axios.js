c178ab193157786e035b6e38e9d5f3d390b19dcb
NEM-ICU <itt1819025@tec.rjt.ac.lk>
2025-09-02 13:20:21 +0530

    change baseUrl for nginx

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || "http://backend:4000/api/",
-  timeout: 5000,
+  baseURL: "/api/",
+  timeout: 10000,
 });
 
 export default api;
