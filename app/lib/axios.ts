ae61c85e501d9e195a0d3cc2e4cb106279b1af7a
Nethsara <73237356+NEM-ICU@users.noreply.github.com>
2025-12-03 22:18:32 +0530

    update BASE_URL

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("Base URL:", process.env.NEXT_PUBLIC_BASE_URL);
 
 const api = axios.create({
-  baseURL: "https://intunetoolbackend.sistena.co.uk/",
+  baseURL: "https://intunetoolbackend.sistena.co.uk/api",
   timeout: 10000,
 });
 
